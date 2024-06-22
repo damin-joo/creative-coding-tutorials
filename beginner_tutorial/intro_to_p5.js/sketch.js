9cc6ebcfe066323faaacc233bdd08e4eb60061f9
Damin <damin.joo0725@gmail.com>
2024-06-22 01:43:44 -0400

    finished swing effect tutorial

---
+++ beginner_tutorial/intro_to_p5.js/sketch.js
@@ -0,0 +1,37 @@
+function setup(){
+    // Setup code goes here - only init
+    // TODO: Create a canvas that is 400px wide and 400px tall
+
+    createCanvas(400, 400);     // create canvas (from library: w, h)
+
+    background(Math.floor(Math.random() * 256));    // Sets canvas background (black: 0- white: 255) - greyscale
+    // background(r, g, b);
+    // background('DodgerBlue');     // Named SVG/CSS color string
+    // background('#FF8C00');   // hex
+    // other: grey with alpha, rbg with alpha
+  
+  }
+  
+  function draw(){
+    // Drawing code goes here
+    
+    // point(100, 200);    // x, y
+
+    // line(20, 40, 320, 120);     // x1, y1, x2, y2
+
+    // rect(250, 100, 100, 100);     // x, y, w, h
+
+    // square(250, 100, 100);    // x, y, w
+
+    // ellipse(150, 200, 150, 100);      // x, y, w, h
+
+    // circle(300, 150, 75);     // x, y, w
+
+    // triangle(0, 0, 100, 100, 0, 100);    // x1, y1, x2, y2, x3, y3
+
+    // quad(30, 10, 60, 20, 60, 30, 20, 30);     // x1, y1, x2, y2, x3, y3, x4, y4
+
+
+
+    
+  }
